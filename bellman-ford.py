b549b74e3886e14ed2c21b9edc5c5f506e5a596b
zhangsr <runningmanzzz@163.com>
2021-10-25 12:39:59 +0800

    1.0-testdata

--- bellman-ford.py
+++ bellman-ford.py
@@ -1,15 +1,15 @@
 # Created by Zhang Siru at 2021/10/22 15:19 with PyCharm
 # Feature: Using Bellman-Ford Algorithm to find the shortest path or secondary shortest path
 # Python Version: 3.6
-# SourceFile: ./data/graphgenerated.csv
+# ResourceFile: ./data/graphgenerated.csv
 
 """
 Bellman-Ford Algorithm:
 已知点边关系
-用邻接矩阵求解？
-如何处理Pr(v)的选择问题？
-初始化随机选择点？直接指定0？
-考虑负权重，如何初始化∞的cost
+用邻接矩阵求解？可以
+如何处理Pr(v)的选择问题？先读完文件，可以获得顶点数，再从中随机选择一个，[实验过程可以换成for循环遍历每一个可能的源点]
+初始化随机选择点？直接指定0？同上
+考虑负权重，如何初始化∞的cost？float['inf']
 
 """
 
